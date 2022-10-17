6d0f82f88545521dc5b4a218d4d0c69a3f12f359
bekmurod3388 <bekmurod3388@gmail.com>
2022-10-17 16:36:17 +0500

    some changes

--- app/Http/Controllers/Api/SensorController.php
+++ app/Http/Controllers/Api/SensorController.php
@@ -10,12 +10,12 @@ class SensorController extends Controller
 {
     public function store(Request $request){
         $sensor = Sensor::create([
-            'sensor1'=>$request->d1,
-            'sensor2'=>$request->d2,
-            'sensor3'=>$request->d3,
-            'sensor4'=>$request->d4,
-            'sensor5'=>$request->d5,
-            'sensor6'=>$request->d6,
+            'sensor1'=>$request->d2,
+            'sensor2'=>$request->d3,
+            'sensor3'=>$request->d4,
+            'sensor4'=>$request->d5,
+            'sensor5'=>$request->d6,
+            'sensor6'=>$request->d8,
         ]);
     }
 
